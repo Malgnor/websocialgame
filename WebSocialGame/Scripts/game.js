97c8d95405efdedec858fb5267987e8bd79f9b9d
unknown <alex.messias@LCET1121.cwbdis.pucpr.br>
2015-11-17 21:21:54 -0200

    .

--- WebSocialGame/Scripts/game.js
+++ WebSocialGame/Scripts/game.js
@@ -1,1 +1,14 @@
-﻿
+﻿/// <reference path="phaser.js" />
+
+(function () {
+    var game = new Phaser.Game(800, 360, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+    function preload() {
+    }
+
+    function create() {
+    }
+
+    function update() {
+    }
+}());
