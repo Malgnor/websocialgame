16db75a410e5ca04f993897e72df0b2406c9519e
unknown <alex.messias@LTCH0217.cwbdis.pucpr.br>
2015-11-18 21:20:40 -0200

    Player can jump

--- WebSocialGame/Scripts/states.js
+++ WebSocialGame/Scripts/states.js
@@ -17,8 +17,8 @@ function mainMenu() {
 
 function inGame() {
     var inGame = new Phaser.State();
-    var player, enemies, ground, coins, backgrounds;
-    var playerSpeed = 5;
+    var player, enemies, ground, coins, backgrounds,
+    playerSpeed = 5, spacebar;
 
     inGame.create = function () {
         inGame.physics.startSystem(Phaser.Physics.ARCADE);
@@ -29,22 +29,27 @@ function inGame() {
 
         player = inGame.add.sprite(0, 100, 'player');
         inGame.physics.arcade.enable(player);
-        player.body.bounce.y = 0.2;
-        player.body.gravity.y = 300;
+        player.body.gravity.y = 500;
         player.body.collideWorldBounds = true;
         player.animations.add('right', [0, 1, 2, 3, 4, 5, 6, 7], playerSpeed*2, true);
         player.animations.play('right');
 
-        ground = inGame.add.sprite(0, 300, 'ground');
+        ground = inGame.add.sprite(0, 350, 'ground');
         ground.alpha = 0.5;
         inGame.physics.arcade.enable(ground);
         ground.body.immovable = true;
+
+        spacebar = inGame.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
     }
 
     inGame.update = function () {
         inGame.physics.arcade.collide(ground, player);
         backgrounds.forEach(backgroundScroll, this);
         player.animations.getAnimation('right').speed = playerSpeed * 2;
+
+        if (spacebar.isDown && player.body.touching.down) {
+            player.body.velocity.y = -450;
+        }
     }
 
     function backgroundScroll(background) {
