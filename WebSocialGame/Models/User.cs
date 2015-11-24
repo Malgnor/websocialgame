d02ab9f3c6999656ec5ae0ebb4c3bcf999dac5d6
unknown <alex.messias@LCET1116.cwbdis.pucpr.br>
2015-11-24 19:24:02 -0200

    Added entity framework

---
+++ WebSocialGame/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSocialGame.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public int FBID { get; set; }
+        public string Name { get; set; }
+        public int HighestDistance { get; set; }
+        public int HighestCoins { get; set; }
+    }
+}
